374a0e83ca0d34eba8ca656591d3cbfe7b2a5c38
Caio Vinícius <caio1525pereira@gmail.com>
2023-08-15 15:32:08 -0300

    feat: :sparkles: Base de event pattern

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from "@nestjs/core";
+import { MicroserviceOptions, Transport } from "@nestjs/microservices";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3334);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3334,
+        host: "127.0.0.1"
+      }
+    }
+  );
+
+  await app.listen();
 }
 bootstrap();
